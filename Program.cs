3ff4b3c518ebdb74554b0fcdaa3392316c7d0a53
fuaddx <147551899+fuaddx@users.noreply.github.com>
2023-10-19 00:14:07 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { -1, -2, -3, -6, -7, -8 };
+            Array(ref arr);
+        }
+        static void Array(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    arr[i] *= -1;
+                }
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
